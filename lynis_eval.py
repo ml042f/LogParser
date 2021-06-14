f9ea3462e3422783872116dfcff93d8574689e7a
ml042f <ml042f@att.com>
2021-06-14 16:06:39 -0500

    Basic regex added as POC

--- lynis_eval.py
+++ lynis_eval.py
@@ -1,1 +1,19 @@
+import re
 
+# Creates and compiles regular expresions as objects
+testname = 'Performing test ID KRNL-5820'
+end = r'===='
+regex = re.compile(testname + '.*?' + end, re.DOTALL)
+
+# Runs regex against a string
+# s = "This is just\na simple sentence"
+# print(regex.match(s))
+
+logfile = r"lynislogs/cvb_r4.log"
+logtext = open(logfile)
+logstring = logtext.read()
+logtext.close()
+ 
+# print(data)
+testdata = regex.search(logstring)
+print(testdata.group())
