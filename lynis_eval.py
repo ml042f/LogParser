b8dbf0a13408e9acde87f67d87b388247c42f460
ml042f <ml042f@att.com>
2021-06-15 11:55:42 -0500

    Added check_success function

--- lynis_eval.py
+++ lynis_eval.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description='A program to filter results from security scans.')
 parser.add_argument("log_file", help="path to logfile; ex: 'lynislogs/cvb_r4.log'", type=str)
 parser.add_argument("-p", "--phase", help="incubation, maturity, or core; defaults to incubation", default="incubation", type=str)
-parser.add_argument("-t", "--tests", help="json file with tests to check; defaults to test_list.json", default="test_list.json", type=str)
+parser.add_argument("-r", "--requirements", help="json file with tests to check; defaults to test_list.json", default="test_list.json", type=str)
 args = parser.parse_args()
 
 def make_raw_string(path_to_file):
@@ -26,13 +26,21 @@ def read_log_to_string(path_to_log):
 
 def load_requirements(path_to_requirements):
     '''returns dictionary object of json object specified in path:
-    pathToRequirements is a string (test_list.json)
+    path_to_requirements is a string (test_list.json)
     '''
     raw_path = make_raw_string(path_to_requirements)
     reqs_file = open(raw_path)
     reqs = json.load(reqs_file)
     reqs_file.close()
     return reqs
+
+def check_success(testname, log_entry):
+    if 'assigned partial' in log_entry.group():
+        print(testname + ": FAILED")
+        return True
+    else:
+        print(testname + ": SUCCESS")
+        return False
 
 def main(log_path, phase, req_path):
     '''Takes log, phase, and requirements documents to find important failing tests
@@ -43,13 +51,14 @@ def main(log_path, phase, req_path):
     re.escape(r'\ a.*$')
     # Creates and compiles regular expressions as objects
     for testname in requirements['lynis'][phase]:
-        testname = re.escape(testname)
+        testname_escaped = re.escape(testname)
         end = r'===='
-        regex = re.compile(testname + '.*?' + end, re.DOTALL)
-
+        regex = re.compile(testname_escaped + '.*?' + end, re.DOTALL)
         # Runs regex against a string
         testdata = regex.search(log_string)
-        print(testdata.group())
+        if check_success(testname, testdata):
+            print('FillerText')
+
 
 if __name__ == "__main__":
-    main(args.log_file, args.phase, args.tests)
+    main(args.log_file, args.phase, args.requirements)
