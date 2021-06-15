c0c3b69d1534bac0496a19628c3db2d9d4487632
ml042f <ml042f@att.com>
2021-06-15 09:26:51 -0500

    Added escape for special regex characters

--- lynis_eval.py
+++ lynis_eval.py
@@ -1,19 +1,22 @@
 import re
 
-# Creates and compiles regular expresions as objects
-testname = 'Performing test ID KRNL-5820'
-end = r'===='
-regex = re.compile(testname + '.*?' + end, re.DOTALL)
-
-# Runs regex against a string
-# s = "This is just\na simple sentence"
-# print(regex.match(s))
-
+# Reads in logfile
 logfile = r"lynislogs/cvb_r4.log"
 logtext = open(logfile)
 logstring = logtext.read()
 logtext.close()
- 
-# print(data)
-testdata = regex.search(logstring)
-print(testdata.group())
+
+re.escape(r'\ a.*$')
+
+listoftests = ['Test: Checking PASS_MAX_DAYS option in /etc/login.defs', 'Performing test ID AUTH-9328 (Default umask values)', 'Performing test ID SSH-7440 (Check OpenSSH option: AllowUsers and AllowGroups)', 'Test: checking for file /etc/network/if-up.d/ntpdate', 'Result: sysctl key fs.suid_dumpable', 'Result: sysctl key kernel.dmesg_restrict', 'Result: sysctl key net.ipv4.conf.default.accept_source_route', 'Performing test ID HRDN-7220 (Check if one or more compilers are installed)']
+
+# Creates and compiles regular expressions as objects
+for testname in listoftests:
+    # testname = r'Performing test ID AUTH-9328 (Default umask values)'
+    testname = re.escape(testname)
+    end = r'===='
+    regex = re.compile(testname + '.*?' + end, re.DOTALL)
+
+    # Runs regex against a string
+    testdata = regex.search(logstring)
+    print(testdata.group())
