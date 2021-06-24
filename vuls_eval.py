1fdb026a5c7ae9fa5d989691c1c0ed8d82826177
ml042f <ml042f@att.com>
2021-06-24 15:16:18 -0500

    Verify correct number of logs, no duplicates

--- vuls_eval.py
+++ vuls_eval.py
@@ -8,12 +8,20 @@ import load_files
 
 parser = argparse.ArgumentParser(description='A program to filter results from security scans.')
 parser.add_argument("log_file", help="path to logfile; ex: 'vuls_logs/cvb_r4.log'", type=str)
-# parser.add_argument("-p", "--phase", help="incubation, maturity, or core; defaults to incubation", default="incubation", type=str)
 parser.add_argument("-r", "--requirements", help="json file with tests to check; defaults to test_list.json", default="test_list.json", type=str)
 parser.add_argument("-b", "--blueprint_name", help="input blueprint name", type=str)
 parser.add_argument("-l", "--logging", help="set this flag to enable writing to log vuls_eval_[blueprintname]_[date_time].log", action='store_true')
 
 args = parser.parse_args()
+
+def check_mult_logs(log_list):
+    cve_count = 0
+    for line in log_list:
+        if("CVE-ID" in line):
+            cve_count += 1
+    if cve_count > 1:
+        return True
+
 
 def get_os(log):
     regex = re.compile(r"centos\d+\.\d*|ubuntu\d+\.\d*")
@@ -34,11 +42,15 @@ def check_cvss(log_lines):
             cve_list.append(cve.group())
     return cve_list
 
-def main(log_path, phase, req_path, blueprint_name, logging):
+def main(log_path, req_path, blueprint_name, logging):
     log_string = load_files.load_log(log_path)
     log_list = load_files.load_log(log_path, False)
     requirements = load_files.load_requirements(req_path)
     
+    if check_mult_logs(log_list):
+        print("Please only submit logs with one log")
+        return 0
+
     now = datetime.now().strftime("%Y_%m_%d_%H#%M#%S")
     filename = "vuls_eval_" + blueprint_name + "_" + now +".log"
     if(logging):
@@ -54,12 +66,10 @@ def main(log_path, phase, req_path, blueprint_name, logging):
         else:
             print(cve + ": failed in scan: NO EXCEPTION")
 
-
-
     if(logging):
         log_file.close()
         sys.stdout = sys.__stdout__  
 
 
 if __name__ == "__main__":
-    main(args.log_file, args.phase, args.requirements, args.blueprint_name, args.logging)
+    main(args.log_file, args.requirements, args.blueprint_name, args.logging)
