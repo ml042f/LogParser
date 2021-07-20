afd22589e5c060dd3a883e4181dfbdf74ac38451
Loduca, Maxwell (ml042f) <ml042f@att.com>
2021-07-20 12:48:46 -0500

    added directory creation to runall.sh

--- runall.sh
+++ runall.sh
@@ -4,3 +4,8 @@
 ./vuls_eval.py $1os/vuls/vuls.log -b $2 -l
 ./kube_eval.py $1k8s/kube-hunter/Kube-Hunter.Kube-Hunter/cluster.log -b $2 -c -l
 ./kube_eval.py $1k8s/kube-hunter/Kube-Hunter.Kube-Hunter/pod.log -b $2 -l
+
+mkdir $2
+mv lynis_eval_* $2
+mv vuls_eval_* $2
+mv kube_eval_* $2
