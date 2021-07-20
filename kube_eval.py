a6cef5bcf608bc920f9027e4ee68b11905bf7fd9
Loduca, Maxwell (ml042f) <ml042f@att.com>
2021-07-20 11:36:27 -0500

    added support for cluster and log in kube_eval

--- kube_eval.py
+++ kube_eval.py
@@ -10,7 +10,8 @@ parser.add_argument("log_file", help="path to logfile; ex: 'kubelogs/cvb_r4.log'
 parser.add_argument("-p", "--phase", help="incubation, maturity, or core; defaults to incubation", default="incubation", type=str)
 parser.add_argument("-r", "--requirements", help="json file with tests to check; defaults to test_list.json", default="test_list.json", type=str)
 parser.add_argument("-b", "--blueprint_name", help="input blueprint name", type=str)
-parser.add_argument("-l", "--logging", help="set this flag to enable writing to log kube_eval_[blueprintname]_[date_time].log", action='store_true')
+parser.add_argument("-c", "--cluster", help="set if you are running for a cluster", action='store_true')
+parser.add_argument("-l", "--logging", help="set this flag to enable writing to log kube_eval_[podorcluster]_[blueprintname]_[date_time].log", action='store_true')
 args = parser.parse_args()
 
 
@@ -32,15 +33,21 @@ def get_vulns(log_list):
             vuln_list.append(vuln)
     return vuln_list
 
-def main(log_path, phase, req_path, blueprint_name, logging):
+def main(log_path, phase, req_path, blueprint_name, logging, cluster):
     '''Takes log, phase, and requirements documents to find important failing tests'''
 
     log_string = load_files.load_log(log_path)
     log_list = load_files.load_log(log_path, False)
-    requirements = load_files.load_requirements(req_path)
+    requirements = load_files.load_requirements(req_path)\
+        
 
     now = datetime.now().strftime("%Y_%m_%d_%H#%M#%S")
-    filename = "kube_eval_" + blueprint_name + "_" + now +".log"
+
+    if(cluster):
+        filename = "kube_eval_" + blueprint_name + "_" + now + "_cluster" + ".log"
+    else:
+        filename = "kube_eval_" + blueprint_name + "_" + now + "_pod" + ".log"
+
     if(logging):
         log_file = open(filename, "x")
         sys.stdout = log_file
@@ -62,4 +69,4 @@ def main(log_path, phase, req_path, blueprint_name, logging):
 
 
 if __name__ == "__main__":
-    main(args.log_file, args.phase, args.requirements, args.blueprint_name, args.logging)
+    main(args.log_file, args.phase, args.requirements, args.blueprint_name, args.logging, args.cluster)
